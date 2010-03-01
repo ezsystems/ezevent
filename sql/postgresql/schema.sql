409e902b6f47d439e32941f2846d7f9efb9eed26
Jérôme Renard <jr@ez.no>
2010-03-01 14:35:55 +0000

    - Added Postgresql schema
    git-svn-id: http://svn.projects.ez.no/ezevent/trunk@8 7cf8869f-158e-43d8-a1a4-8acddba05948

---
+++ sql/postgresql/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE ezevent (
+  ID                         SERIAL   NOT NULL,
+  parent_event_id            INT   NOT NULL,
+  contentobject_attribute_id INT   NOT NULL,
+  version                    INT   NOT NULL,
+  start_date                 INT   NOT NULL,
+  end_date                   INT   NOT NULL,
+  event_type                 SMALLINT   DEFAULT '14'   NOT NULL,
+  is_parent                  SMALLINT   DEFAULT '0'   NOT NULL,
+  is_temp                    SMALLINT   DEFAULT '0'   NOT NULL,
+  CONSTRAINT pk_ezevent PRIMARY KEY ( id,contentobject_attribute_id,version ));
+
+CREATE INDEX contentobject_attribute_id ON ezevent (
+      contentobject_attribute_id,
+      version);
+
+CREATE INDEX parent_event_id ON ezevent (
+      parent_event_id);
