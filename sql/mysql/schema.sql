bcd5ec9b7ed9c0d83c7f5ca3b28574ae17ff7283
Jérôme Renard <jr@ez.no>
2010-03-01 14:35:35 +0000

    - Updated MySQL schema
    git-svn-id: http://svn.projects.ez.no/ezevent/trunk@7 7cf8869f-158e-43d8-a1a4-8acddba05948

--- sql/mysql/schema.sql
+++ sql/mysql/schema.sql
@@ -1,18 +1,14 @@
-DROP TABLE IF EXISTS ezevent;
-CREATE TABLE ezevent
-(
-  id                            int(11) NOT NULL AUTO_INCREMENT,
-  parent_event_id               int(11) NOT NULL,
-  contentobject_attribute_id    int(11) NOT NULL,
-  version                       int(11) NOT NULL,
-  start_date                    int(11) NOT NULL,
-  end_date                      int(11) NOT NULL,
-  event_type                    int(2)  default 14,
-  is_parent                     int(1)  default 0,
-  is_temp                       int(1)  default 0,
-  PRIMARY KEY( id, contentobject_attribute_id, version ),
-  KEY( contentobject_attribute_id, version ),
-  KEY( parent_event_id ) -- ,
-  -- CONSTRAINT FOREIGN KEY ( parent_event_id ) REFERENCES ezevent( id ) --
-  -- ON DELETE CASCADE --
-);
+CREATE TABLE ezevent (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  parent_event_id int(11) NOT NULL,
+  contentobject_attribute_id int(11) NOT NULL,
+  version int(11) NOT NULL,
+  start_date int(11) NOT NULL,
+  end_date int(11) NOT NULL,
+  event_type tinyint(4) NOT NULL DEFAULT '14',
+  is_parent tinyint(4) NOT NULL DEFAULT '0',
+  is_temp tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (id,contentobject_attribute_id,version),
+  KEY contentobject_attribute_id (contentobject_attribute_id,version),
+  KEY parent_event_id (parent_event_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
