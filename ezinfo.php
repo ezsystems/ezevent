59e548c1c5628d07e07f887598bc8c11e93dfac5
Bertrand Dunogier <bd@ez.no>
2011-02-19 17:44:04 +0100

    Updated ezinfo.php with proper version number tag

--- ezinfo.php
+++ ezinfo.php
@@ -28,9 +28,9 @@ class ezeventInfo
     static function info()
     {
         return array( 'Name'      => "eZ Publish Event",
-                      'Version'   => "1.0.0",
+                      'Version'   => "//autogentag//",
                       'Copyright' => "Copyright (C) 1999-2010 eZ Systems AS",
-                      'License'   => "GNU General Public License v2.0"
+                      'License'   => "//EZP_LICENSE//"
                     );
     }
 }
