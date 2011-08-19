aa08c3e6b173a7fbd6069d86fefa7ac6a0736c36
Damien Pobel <dp@ez.no>
2011-08-19 15:12:24 +0200

    Fixed a fatal error when installing ezteamroom

--- datatypes/ezevent/ezeventtype.php
+++ datatypes/ezevent/ezeventtype.php
@@ -123,7 +123,7 @@ class eZEventType extends eZDataType
                 if ( !$this->validateDateTimeHTTPInput( $day, $month, $year, $hour, $minute, $contentObjectAttribute, false ) )
                 {
                     $errorOccured = true;
-                    $contentObjectAttribute->setValidationError( ezpI18n::tr(( 'ezevent/datatypes',
+                    $contentObjectAttribute->setValidationError( ezpI18n::tr( 'ezevent/datatypes',
                                                                         'Invalid start date.' ) );
                 }
                 if ( !$this->validateDateTimeHTTPInput( $endDay, $endMonth, $endYear, $endHour, $endMinute, $contentObjectAttribute, false  ) )
