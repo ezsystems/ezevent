b39d7e961fffdbf851ddfa0033eebfa829b6375e
Damien Pobel <dp@ez.no>
2012-03-29 13:48:51 +0200

    Changed: welcome 2012

--- ezinfo.php
+++ ezinfo.php
@@ -4,7 +4,7 @@
 //
 // SOFTWARE NAME: eZ Event extension for eZ Publish
 // SOFTWARE RELEASE: 1.x
-// COPYRIGHT NOTICE: Copyright (C) 1999-2010 eZ Systems AS
+// COPYRIGHT NOTICE: Copyright (C) 1999-2012 eZ Systems AS
 // SOFTWARE LICENSE: GNU General Public License v2.0
 // NOTICE: >
 //   This program is free software; you can redistribute it and/or
@@ -29,7 +29,7 @@ class ezeventInfo
     {
         return array( 'Name'      => "eZ Publish Event",
                       'Version'   => "//autogentag//",
-                      'Copyright' => "Copyright (C) 1999-2010 eZ Systems AS",
+                      'Copyright' => "Copyright (C) 1999-2012 eZ Systems AS",
                       'License'   => "GNU General Public License v2.0"
                     );
     }
